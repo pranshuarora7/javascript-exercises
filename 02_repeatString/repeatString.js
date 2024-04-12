ecf27d8e8e5fda83e59bd63f99e7a72303124edd
Pranshu Arora <pranshuarora1618@gmail.com>
2024-04-12 20:13:33 +0530

    completed 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let a = string;
+    let i = 0;
+    if (num > 0) {
+        for (i = 0; i < num - 1; i++) {
+            a += string;
+        }
+        return a;
+    }
+    else if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        return "";
+    }
 };
 
 // Do not edit below this line
